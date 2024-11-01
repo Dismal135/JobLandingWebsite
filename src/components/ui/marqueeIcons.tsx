386504e51fcf75a7e64b68a60dfc52ca39769861
unknown <phyothiha97531@gmail.com>
2024-11-01 12:20:46 +0630

    'fix'

--- src/components/ui/marqueeIcons.tsx
+++ src/components/ui/marqueeIcons.tsx
@@ -8,17 +8,12 @@ import Image from "next/image";
 const MarqueeIcons = () => {
     return (
         <section className='overflow-hidden'>
-          <div className='flex gap-4 marquee lg:animate-none lg:mx-auto w-[1043px]'>
+          <div className='flex gap-4 marquee lg:animate-none lg:mx-auto w-[1040px'>
           <Image src={nextjs} width={160} height={100} alt="icon" />
           <Image src={react} width={302} height={100} alt="icon" />
           <Image src={shadcn} width={252} height={100} alt="icon" />
           <Image src={ffc} width={148} height={100} alt="icon" />
           <Image src={w3schools} width={100} height={100} alt="icon" />
-          <Image className='lg:hidden' src={nextjs} width={160} height={100} alt="icon" />
-          <Image className='lg:hidden' src={react} width={302} height={100} alt="icon" />
-          <Image className='lg:hidden' src={shadcn} width={252} height={100} alt="icon" />
-          <Image className='lg:hidden' src={ffc} width={148} height={100} alt="icon" />
-          <Image className='lg:hidden' src={w3schools} width={100} height={100} alt="icon" />
           </div>
         </section>
     )
