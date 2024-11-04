ff6ff8b3ad6f95564db8b7d60cf68977fa2d1be1
PHYO THI HA <phyothiha97531@gmail.com>
2024-11-05 03:59:56 +0630

    marquee

--- src/components/ui/marqueeIcons.tsx
+++ src/components/ui/marqueeIcons.tsx
@@ -8,7 +8,12 @@ import Image from "next/image";
 const MarqueeIcons = () => {
     return (
         <section className='overflow-hidden'>
-          <div className='flex gap-4 marquee lg:animate-none lg:mx-auto w-[1040px]'>
+          <div className='marquee lg:animate-non flex gap-[10px] w-[890px]'>
+          <Image src={nextjs} width={undefined} height={100} alt="icon" />
+          <Image src={react} width={undefined} height={100} alt="icon" />
+          <Image src={shadcn} width={undefined} height={100} alt="icon" />
+          <Image src={ffc} width={undefined} height={100} alt="icon" />
+          <Image src={w3schools} width={undefined} height={100} alt="icon" />
           <Image src={nextjs} width={undefined} height={100} alt="icon" />
           <Image src={react} width={undefined} height={100} alt="icon" />
           <Image src={shadcn} width={undefined} height={100} alt="icon" />
