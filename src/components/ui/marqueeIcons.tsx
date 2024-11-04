83894a9ba7918a8066f07c76ce15c331eb2dab03
PHYO THI HA <phyothiha97531@gmail.com>
2024-11-05 04:37:36 +0630

    marquee

--- src/components/ui/marqueeIcons.tsx
+++ src/components/ui/marqueeIcons.tsx
@@ -4,17 +4,19 @@ import shadcn from '@/app/images/image-72 (1).png';
 import ffc from '@/app/images/ffcicon.png';
 import w3schools from "@/app/images/w3schoolsicon.jpg";
 import Image from "next/image";
+import React from "react";
+import Marquee from "react-fast-marquee";
 
 const MarqueeIcons = () => {
     return (
         <section className='overflow-hidden max-w-[1000px] mx-auto'>
-          <div className='marquee lg:mx-auto flex gap-[10px] w-[880px]'>
+          <Marquee>
           <Image src={nextjs} width={undefined} height={100} alt="icon" />
           <Image src={react} width={undefined} height={100} alt="icon" />
           <Image src={shadcn} width={undefined} height={100} alt="icon" />
           <Image src={ffc} width={undefined} height={100} alt="icon" />
           <Image src={w3schools} width={undefined} height={100} alt="icon" />
-          </div>
+          </Marquee>
         </section>
     )
 };
